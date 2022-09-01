7b20cfc6d75ac5f2e1b4c3fc68fc6e70e6babf44
furkan <ahmetfurkankocak@gmail.com>
2022-09-01 23:08:41 +0300

    ...

--- app/src/main/java/com/furkankocak/omdbmoviesearchapp/screen/MainScreen.kt
+++ app/src/main/java/com/furkankocak/omdbmoviesearchapp/screen/MainScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontStyle
